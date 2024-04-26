b6673544d63560a62688302795aff6a45c7af9d0
iqramahamed <iqra.mahamed64@gmail.com>
2024-04-26 16:05:15 +0300

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,12 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const firebrick ={color: 'firebrick'};
+  
+  return <div>Home
+    <h1 style={firebrick}>Your {name} is a Web Developer from Your {city}</h1>
+
+  </div>;
 }
 
 export default Home;
